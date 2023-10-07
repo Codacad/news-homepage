0b35e84fa9142bb6b5560ecb282c192eff7a2a26
Mohd Rizwan <iamrizwan40@gmail.com>
2023-10-07 11:06:10 +0530

    remove repo name from homepage

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React from 'react'
 import Navbar from './components/Navbar'
-import {Routes, Route } from 'react-router-dom'
 import Home from './components/Home'
 import './css/App.css'
 const App = () => {
   return (
     <div className='app'> 
         <Navbar />
-        <Routes>
-          <Route path='/' element={<Home />}/>
-        </Routes>
+        <Home />
     </div>
   )
 }
